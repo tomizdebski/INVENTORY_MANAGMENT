3e40a2e8a23300a46e163685eb73fc12c4294d6d
tomizdebski <tomizdebski@gmail.com>
2024-08-17 13:50:10 +0200

    add-few-icons

--- app/(components)/Navbar/index.tsx
+++ app/(components)/Navbar/index.tsx
@@ -32,6 +32,11 @@ const Navbar = () => {
                     <Sun className="cusor-pointer text-gray-500" size={24}/>
                 </button>
             </div>
+            <div className="relative">
+                <Bell className="cusor-pointer text-gray-500" size={24}/>
+                <span className="absolute -top-2 -right-2 inline-flex items-center justify-center px-[0.4rem] py-1 text-xs font-semibold leading-none text-red-100 bg-red-400 rounded-full">2</span>
+            </div>
+            <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3"/>
         </div>
       </div>
     </div>
